4729d84f1792f4eb31e2b3a39f234f94edfd264b
Hérikles Vinícyus <heriklesvinicyus@hotmail.com>
2020-07-18 01:41:51 -0300

    Criação do agente BFS baseado no DFS

---
+++ Atividade_04_BFS/Atividade_04_BFS.pyde
@@ -0,0 +1,106 @@
+from Grid import Grid 
+from Food import Food
+from Agente import Agente
+
+import math
+
+#conf tela
+altura_grid = 500
+largura_grid = 500
+quant_hor = 21#colocar igual a ver(erro serah concertado)
+quant_ver = 21#colocar igual a hor(erro serah concertado)
+
+tela_auxiliar = largura_grid*0.4
+
+altura_tela = int(math.ceil(altura_grid/quant_ver)*quant_ver+50)
+largura_tela = int(math.ceil(largura_grid/quant_hor)*quant_hor + tela_auxiliar)
+
+def setup():    
+    global g, f, a
+
+    g = Grid(altura_grid,largura_grid,quant_hor,quant_ver)
+     
+    aux = g.procurar_posicao_vazia()
+    f = Food(aux[0],aux[1])
+    
+    #configurado para agente iniciar no meio do grid
+    a = Agente(g.nodes[int(math.ceil(quant_hor/2))][int(math.ceil(quant_ver/2))])
+        
+    #Muda a velocidade de que atualiza draw
+    frameRate(10)
+    
+    size(largura_tela, altura_tela)
+    background(255)
+    
+def draw():
+    global f
+    
+    #pontuação
+    fill(255)
+    rect(0,height-50, largura_tela,height-50)
+    textSize(25)
+    fill(0, 0, 0)
+    text('Pontos {}'.format(a.pontos), 10, height-15)
+    
+    #variavel temporaria para auxiliar a funcao de busca do alimento;
+    aux_node = g.retornar_node(a.atual.i, a.atual.j)
+    a.buscar_comida(aux_node[0], aux_node[1])
+    
+    if(a.achou_comida): 
+        f.dead()
+        
+    if(f.is_dead):
+        aux = g.procurar_posicao_vazia()
+        f = Food(aux[0],aux[1])
+        g.posicao_comida_node(f.i,f.j)
+        a.achou_comida = False
+        
+    grid_secundario(a.nodes_abertos, a.nodes_fechados)
+    g.pintar_nodes_fechados(a.nodes_fechados)
+    g.pintar_nodes_abertos(a.nodes_abertos)
+    g.pintar_nodes_atual(a.atual)
+    g.pintar_proximo_node_anilizado(a.nodes_abertos[-1])
+    g.posicao_comida_node(f.i,f.j)
+    g.posicao_agente(a.i,a.j)
+    g.display()
+
+#melhor nome
+def grid_secundario(abertos, fechados):
+    x_tela_aux = largura_tela-tela_auxiliar
+    fill(255)
+    rect(x_tela_aux, 0,tela_auxiliar ,height-50)
+    '''
+    #pilha fechados
+    fill(0)
+    rect(x_tela_aux+20, 10, tela_auxiliar*0.3, height-70)
+        
+    #pilha abertas
+    fill(0)
+    rect(x_tela_aux+120, 10, tela_auxiliar*0.3, height-70)
+    '''
+    
+    aux_y = height-70-tela_auxiliar*0.1
+    fill(0)
+    textSize(16)
+    text('F', x_tela_aux+23, aux_y+tela_auxiliar*0.2)
+    for n,x in enumerate(fechados):
+        if (len(fechados)>22) and (n < len(fechados)-22):
+            continue
+        textSize(16)
+        fill(0)
+        text('{}:{},{}'.format(n,x.i,x.j), x_tela_aux+23, aux_y+tela_auxiliar*0.1)
+        aux_y -= tela_auxiliar*0.1
+    
+    aux_y = height-70-tela_auxiliar*0.1
+    text('A', x_tela_aux+123, aux_y+tela_auxiliar*0.2)
+    for n,x in enumerate(abertos):
+        if (len(abertos)>22) and (n < len(abertos)-22):
+            continue
+        textSize(16)
+        fill(0)
+        text('{}:{},{}'.format(n,x.i,x.j), x_tela_aux+123, aux_y+tela_auxiliar*0.1)
+        aux_y -= tela_auxiliar*0.1
+    
+    
+    
+        
---
+++ Atividade_04_BFS/Grid.py
@@ -0,0 +1,142 @@
+from Node import Node
+import math
+import random
+
+class Grid():
+    def __init__(self, altura, largura, quant_hor, quant_ver):
+        self.altura = altura
+        self.largura = largura
+        #x
+        self.quant_hor = quant_hor
+        #y
+        self.quant_ver = quant_ver
+        
+        self.nodes = []
+        
+        #Rotina de inicializacao
+        self.__iniciar_matrix_nodes()
+        self.__adicionar_obstaculos()
+        self.__adicionar_nodes_visiveis()
+                        
+    def posicao_agente(self,i,j):
+        self.nodes[i][j].mudar_valor(1)
+    
+    def posicao_comida_node(self,i, j):
+        self.nodes[i][j].mudar_valor(2)
+    
+    def pintar_nodes_fechados(self, array_fechados):#TUDO Mudar para nome melhor
+        for x in array_fechados:
+            self.nodes[x.i][x.j].mudar_valor(4)
+            
+    def pintar_nodes_abertos(self, array_abertos):#TUDO Mudar para nome melhor
+        for x in array_abertos:
+            self.nodes[x.i][x.j].mudar_valor(3)
+            
+    def pintar_nodes_atual(self, atual):#TUDO Mudar para nome melhor
+        self.nodes[atual.i][atual.j].mudar_valor(5)
+        
+    def pintar_proximo_node_anilizado(self, prox):#TUDO Mudar para nome melhor
+        self.nodes[prox.i][prox.j].mudar_valor(6)
+    
+    def retornar_node(self,i,j):
+        return [self.nodes[i][j].valor,self.nodes[i][j].nodes_visiveis]
+        
+    def procurar_posicao_vazia(self):
+        self.__limpar_fechados_abertos_comida()
+        while(True):
+            i = random.randrange(0, self.quant_ver)
+            j = random.randrange(0, self.quant_hor)
+            if(self.nodes[i][j].valor == 0 and i != int(math.ceil(self.quant_hor/2)) and j != int(math.ceil(self.quant_ver/2))):
+                return [i,j]
+            
+    #Rotinas de inicializacao>>
+    def __iniciar_matrix_nodes(self):
+        
+        for i in range(self.quant_ver):
+            aux = []
+            for j in range(self.quant_hor):
+                aux.append(Node(i, j))
+            self.nodes.append(aux)
+            
+    def __adicionar_obstaculos(self):
+        for i in range(int((self.quant_hor * self.quant_ver)*0.2)):
+            aux = self.procurar_posicao_vazia()
+            self.nodes[aux[0]][aux[1]].mudar_valor(-1)
+            
+    #Dentro do node existe uma variavel com os nodes que ela conhece. essa funcao adiciona os valores dos nodes visiveis
+    def __adicionar_nodes_visiveis(self):
+        for i in range(len(self.nodes)):
+            for j in range(len(self.nodes[i])):
+                aux = []
+                #verifica se tem no a direita
+                if(j+1 < self.quant_hor and self.nodes[i][j+1].valor != -1):
+                    aux.append(self.nodes[i][j+1])
+                #verifica se tem no a cima
+                if(i-1 > -1 and self.nodes[i-1][j].valor != -1):
+                    aux.append(self.nodes[i-1][j])
+                #verifica se tem no a esqueda
+                if(j-1 > -1 and self.nodes[i][j-1].valor != -1):
+                    aux.append(self.nodes[i][j-1])
+                #verifica se tem no a baixo
+                if(i+1 < self.quant_ver and self.nodes[i+1][j].valor != -1):
+                    aux.append(self.nodes[i+1][j])
+
+                self.nodes[i][j].add_array_nodes_visiveis(aux)
+                
+    def __limpar_fechados_abertos_comida(self):
+        for i in range(len(self.nodes)):
+            for j in range(len(self.nodes[i])):
+                if (self.nodes[i][j].valor in [1,2,3,4]):
+                    self.nodes[i][j].mudar_valor(0)        
+    #Rotinas<<
+    
+    def display(self):
+        aux_altura = math.ceil(self.altura/self.quant_ver)
+        aux_largura = math.ceil(self.largura/self.quant_hor)
+        aux_y = 0
+        for i in self.nodes:
+            aux_x = 0
+            for j in i:
+                #valor indica que tem parede (Preto)
+                if(j.valor == -1):
+                    fill(0)
+                    
+                #valor indica que estah vazio (Branco)
+                elif(j.valor == 0):
+                    fill(255)
+                    
+                #valor indica onde estah o agente (Verde)
+                elif(j.valor == 1):
+                    fill(0,200,0)
+                    
+                #valor indica que tem comida (Vermelha)
+                elif(j.valor == 2):
+                    fill(200,0,0)
+                    
+                #valor indica que noh fechado (Roxo)
+                elif(j.valor == 4):
+                    fill(128,0,128)
+                    
+                #valor indica que noh aberto (Ciano)
+                elif(j.valor == 3):
+                    fill(0,255,255)
+                
+                #valor indica qual serah o proximo noh
+                elif(j.valor == 5):
+                    fill(243,156,18)
+                
+                #valor indica qual eh o noh atual de observado
+                elif(j.valor == 6):
+                    fill(3,155,229)
+                    
+                rect(aux_x, aux_y, aux_largura, aux_altura)   
+                aux_x += aux_largura
+            aux_y += aux_altura
+    #
+    def __str__(self):
+        aux = ''
+        for x in self.nodes:
+            for y in x:
+                aux += '{:>3}'.format(y.valor)
+            aux += '\n'
+        return aux    
