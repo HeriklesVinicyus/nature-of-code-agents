de1a7a9618358598d5f3b3dc06b8ad8077870d47
Inácio Nery <inacio.nery@liferay.com>
2020-07-20 21:12:19 -0300

    Use actual node

--- Atividade_04_BFS/Agente.py
+++ Atividade_04_BFS/Agente.py
@@ -12,30 +12,26 @@ class Agente:
         self.nodes_fechados = []
         self.nodes_abertos = [nodo_inicial]
         self.atual = nodo_inicial
-        
-    #algoritmo de busca
-    def buscar_comida(self, conteudo, array_nodes_visiveis):
+
+    # algoritmo de busca
+    def buscar_comida(self):
         '''
         array_nodes_visiveis = nodos visiveis do node atual
         '''
         # verifica se no node tem comida(valor 2)
-        if(conteudo == 2):
+        if(self.atual.valor == 2):
             self.i = self.atual.i
             self.j = self.atual.j
             self.__limpar_fechados_aberto()
             self.achou_comida = True
             self.pontos += 1
             return self.achou_comida
-            
-        aux_pilha_nodes = array_nodes_visiveis
-        for x in aux_pilha_nodes:
-            if(x not in self.nodes_fechados) and (x not in self.nodes_abertos):
-                self.nodes_abertos.append(x)
-                
-        self.nodes_fechados.append(self.atual)        
-        self.atual = self.nodes_abertos.pop(0) if len(self.nodes_abertos)>0 else self.atual
-        
-        
+
+        for childNode in self.atual.nodes_visiveis:
+            if(childNode.valor != -1 and childNode not in self.nodes_fechados) and (childNode not in self.nodes_abertos):
+                self.nodes_abertos.append(childNode)
+
+        self.nodes_fechados.append(self.atual)
         return self.achou_comida
 
     # Rotinas >>
